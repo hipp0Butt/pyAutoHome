2f8970eaae7ee93d712fb9ce4b04e97751ca30de
hipp0Butt <wakeboard115@gmail.com>
2012-05-20 23:35:29 -0700

    Fixed some issues with lightControl.  Added return codes.  Added a simple host-side socket server to allow remote management/access.

--- lightControl.py
+++ lightControl.py
@@ -3,28 +3,41 @@ import sys
 import getopt
 import os
 
+HOME_AUTO_BASE = 0x00
+HOME_AUTO_SUCCESS = HOME_AUTO_BASE
+HOME_AUTO_FAIL_NODEVICE = HOME_AUTO_BASE + 1
+HOME_AUT0_FAIL_UNKNOWN_OPTION = HOME_AUTO_BASE + 2
+
+
 try:
 	import serial
 except:
-	print 'You are missing the pySerial package.  Please install from: ',
+	print 'You are missing the pySerial package.  Please install from:\n',
 	print 'http://sourceforge.net/projects/pyserial/files/pyserial/2.5/'
 
 
 def turnLightsOn(device):
-	sendSerial(device, "61")
+	ret = HOME_AUTO_SUCCESS
+	ret = sendSerial(device, "61")
+	return ret
 
 def turnLightsOff(device):
-	sendSerial(device, "71")
+	ret = HOME_AUTO_SUCCESS
+	ret = sendSerial(device, "71")
+	return ret
 
 def sendSerial(dev, msg):
+	ret = HOME_AUTO_SUCCESS
 	try:
 		ser = serial.Serial(dev)
 		ser.write(msg)
 		ser.close()
 	except IOError:
 		print 'Could not send message.  Make sure device is properly connected'
+		ret = HOME_AUTO_FAIL_NODEVICE
+	return ret
 
-def printHelp():
+def _printHelp():
 	print 'Simple python script to control the Wunderboard serial light controller.'
 	print 'Usage: \n'
 	print 'lightControl.py [-h] [-s on|off] [-d devPath] [--set on|off] [--device devPath]'
@@ -34,9 +47,10 @@ def printHelp():
 	print '-d, --device       : Specify a device to use.  Defaults to /dev/ttyUSB0'
 	print ''
 
-
-if __name__ == "__main__":
+def _main():
 	state = None
+	ret = HOME_AUTO_SUCCESS
+	#print ret
 
 	# figure out the default device based on the executing platform
 	if os.name == 'posix':
@@ -50,25 +64,36 @@ if __name__ == "__main__":
 	
 	for opt, arg in opts:
 		if opt in ('-h', '--help'):
-			printHelp()
-			sys.exit(0)
+			_printHelp()
+			sys.exit(HOME_AUTO_SUCCESS)
 		elif opt in ('-s', '--set'):
 			state = arg
 		elif opt in ('-d', '--device'):
-			if os.name = 'nt':
+			if os.name == 'nt':
 				device = int(arg)
 			else:
 				device = arg
 		else:
 			print 'Unknown option: %s' % opt
+			_printHelp()
+			sys.exit(HOME_AUT0_FAIL_UNKNOWN_OPTION)
 
 		if device == 'unknown':
 			print 'Your serial device could not be auto-detected.  Please specify the correct device',
 			print 'using the --device option, and run lightControl.py again.'
-			sys.exit(1)
+			sys.exit(HOME_AUTO_NODEVICE)
 
 
 	if state.lower() == 'on':
-		turnLightsOn(device)
+		ret = turnLightsOn(device)
 	elif state.lower() == 'off':
-		turnLightsOff(device)
+		ret = turnLightsOff(device)
+	#print ret
+		
+	return ret
+
+if __name__ == "__main__":	
+	ret = _main()
+	#print ret
+	sys.exit(ret)
+
---
+++ server.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+
+import socket
+import os
+import sys
+import signal
+import select
+import pickle
+import commands
+
+HOST = socket.gethostname()
+PORT = 6969
+BACKLOG = 5
+LOGFILE = './server.log'
+BUFSIZE = 1024
+
+# Stuff used for packet structure
+HEAD = 0
+OPCODE = 1
+
+# Possible values for HEAD
+LIGHTS = '0'
+
+# OPCODES for LIGHTS
+COMMAND_LIGHTS_ON = '0'
+COMMAND_LIGHTS_OFF = '1'
+
+running = True
+logfile = None
+
+def log(file, string):
+	file.write(string)
+	file.write('\n')
+	print string
+
+def handler(signum, frame):
+	log(logfile, 'Interrupt detected.  Stopping server')
+	global running
+	running = False
+
+def runSystemCall(call):
+	ret = os.system(call)
+	if ret != 0:
+		log(logfile, 'System call failed: ' + call)
+	return ret
+
+def processPacket(packet):
+	ret = 0
+	# Defines a light control packet
+	# format is: [ HEAD, OPCODE, ...]
+	if packet[HEAD] == LIGHTS:
+		if packet[OPCODE] == COMMAND_LIGHTS_ON:
+			# Code to turn on the lights
+			print 'Lights on'
+			ret = runSystemCall('./lightControl.py -s on')
+		elif packet[OPCODE] == COMMAND_LIGHTS_OFF:
+			# Code to turn off the lights
+			print 'Lights off'
+			ret = runSystemCall('./lightControl.py -s off')
+		else:
+			log(logfile, "Unknown option for light control: " + str(packet[OPCODE]))
+	else:
+		log(logfile, "Uknown HEAD option: " + packet[HEAD])	
+	return ret
+
+def sendPacket(packet, sock):
+	# TODO: Enable the sending of packets
+	log(logfile, 'Sending packets is not yet implemented')
+
+def main():
+	global logfile
+	global running
+	data = ''
+	recv = True
+	
+	# Open the logfile
+	logfile = open(LOGFILE, 'w')
+
+	# Set the signal handler
+	signal.signal(signal.SIGINT, handler)
+	signal.signal(signal.SIGQUIT, handler)
+	signal.signal(signal.SIGHUP, handler)
+	
+	# Configure the socket
+	serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	serv.bind((HOST, PORT))
+	serv.listen(BACKLOG)
+	
+	log(logfile, "Server listening on port: " + str(PORT))
+
+	pot_readers = [serv]
+	
+	while running:
+		#try:
+			# Check for clients using select
+			ready_to_read, ready_to_write, execption \
+			    = select.select(pot_readers, [], [])
+			for x in ready_to_read:
+				if x == serv:
+					# Handle a new client connection
+					conn, addr = serv.accept()
+					pot_readers.append(conn)
+					log(logfile, "Accepting new client: " + str(conn.getpeername()))
+				else:
+					# This is coming from a client that we know about.  Figure out
+					# where it's coming from and what they want
+					data = x.recv(BUFSIZE)
+					if data:
+						while (data[-1] != '\n'):
+							# Make sure we have the whole packet
+							data += x.recv(BUFSIZE)
+						pckt = data.rstrip().split(';')
+						ret = processPacket(pckt)
+						x.send(str(ret) + '\n')
+		#except e:
+		#	log(logfile, "ERROR: " + str(e))
+		#	sys.exit(1)
+	# Shut down the server (should only happen on interrupt)
+	for s in pot_readers:
+		s.close()
+	logfile.close()
+
+
+if __name__ == '__main__':
+	main()
